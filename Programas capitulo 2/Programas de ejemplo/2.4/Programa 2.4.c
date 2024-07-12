4c6737939bf0575bf2c6645a7b4de3c563486bd6
Luis E. Molina <luisemolina07@gmail.com>
2024-07-11 20:16:59 -0400

    Programas de ejemplo capitulo 2

--- Programas capitulo 2/Programas de ejemplo/2.4/Programa 2.4.c
+++ Programas capitulo 2/Programas de ejemplo/2.4/Programa 2.4.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 /* Incremento de precio.
-El programa, al recibir como dato el precio de un producto importado,
-incrementa 11% el mismo si este es inferior a $1500.
+El programa, al recibir como dato el precio de un producto, incrementa al
+mismo 11% si es menor a 1500$ y 8% en caso contrario (mayor o igual).
+
 PRE y NPR: variables de tipo real. */
 
 void main(void)
@@ -10,8 +12,8 @@ void main(void)
     printf("Ingrese el precio del producto: ");
     scanf("%f", &PRE);
     if  (PRE < 1500)
-    {
         NPR = PRE * 1.11;
-        printf("\nNuevo precio: %7.2f",NPR);
-    }
+    else
+        NPR = PRE *1.08;
+    printf("\nNuevo precio del producto: %8.2f", NPR);
 }
